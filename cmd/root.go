4c8256753ba48480af3aae02374c323307cd50b0
William Floyd <william.png2000@gmail.com>
2021-07-23 00:00:30 -0500

    all: More work, just testing.

--- cmd/root.go
+++ cmd/root.go
@@ -22,15 +22,22 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gabriel-vasile/mimetype"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/peterbourgon/diskv/v3"
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -38,6 +45,7 @@ import (
 
 var (
 	cfgFile string
+	d       *diskv.Diskv
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -66,8 +74,16 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.corrugation-backend.yaml)")
 
+	viper.SetEnvPrefix("CORRUGATION")
+
 	rootCmd.Flags().String("jwt", "", "JWT secret")
 	viper.BindPFlag("jwt-secret", rootCmd.Flags().Lookup("jwt"))
+
+	rootCmd.Flags().StringP("username", "u", "", "Login username")
+	viper.BindPFlag("username", rootCmd.Flags().Lookup("username"))
+
+	rootCmd.Flags().StringP("password", "p", "", "Login password")
+	viper.BindPFlag("password", rootCmd.Flags().Lookup("password"))
 
 	rootCmd.Flags().Bool("auth", true, "Enable authentication")
 	viper.BindPFlag("authentication", rootCmd.Flags().Lookup("auth"))
@@ -102,12 +118,45 @@ func initConfig() {
 
 }
 
+func AdvancedTransformExample(key string) *diskv.PathKey {
+	path := strings.Split(key, "/")
+	last := len(path) - 1
+	return &diskv.PathKey{
+		Path:     path[:last],
+		FileName: path[last],
+	}
+}
+
+// If you provide an AdvancedTransform, you must also provide its
+// inverse:
+
+func InverseTransformExample(pathKey *diskv.PathKey) (key string) {
+	return strings.Join(pathKey.Path, "/") + "/" + pathKey.FileName
+}
+
 func server(cmd *cobra.Command, args []string) {
 
-	if viper.GetString("jwt-secret") == "" && viper.GetBool("authentication") {
-		fmt.Fprintln(os.Stderr, "Error:", "JWT Secret must be defined when using auth")
-		os.Exit(1)
+	if viper.GetBool("authentication") {
+		checks := []string{
+			"jwt-secret",
+			"username",
+			"password",
+		}
+		for _, check := range checks {
+			if viper.GetString(check) == "" {
+				fmt.Fprintln(os.Stderr, "Error:", check+" must be defined when using auth")
+				os.Exit(1)
+			}
+		}
 	}
+
+	// Initialize a new diskv store, rooted at "data", with a 16MB cache.
+	d = diskv.New(diskv.Options{
+		BasePath:          "data",
+		AdvancedTransform: AdvancedTransformExample,
+		InverseTransform:  InverseTransformExample,
+		CacheSizeMax:      16 * 1024 * 1024,
+	})
 
 	e := echo.New()
 
@@ -127,6 +176,12 @@ func server(cmd *cobra.Command, args []string) {
 
 	r.GET("/info", info)
 
+	r.POST("/upload/box-image", uploadBoxImage)
+
+	r.GET("/download/box-image", downloadBoxImage)
+
+	r.GET("/list/box-image", listBoxImages)
+
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(viper.GetInt("port"))))
 
 }
@@ -136,7 +191,7 @@ func login(c echo.Context) error {
 	password := c.FormValue("password")
 
 	// Throws unauthorized error
-	if username != "jon" || password != "shhh!" {
+	if username != viper.GetString("username") || password != viper.GetString("password") {
 		return echo.ErrUnauthorized
 	}
 
@@ -145,7 +200,7 @@ func login(c echo.Context) error {
 
 	// Set claims
 	claims := token.Claims.(jwt.MapClaims)
-	claims["name"] = "Jon Snow"
+	claims["name"] = viper.GetString("username")
 	claims["admin"] = true
 	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
@@ -165,4 +220,110 @@ func info(c echo.Context) error {
 	claims := user.Claims.(jwt.MapClaims)
 	name := claims["name"].(string)
 	return c.String(http.StatusOK, "Welcome "+name+"!")
+}
+
+// chanToSlice reads all data from ch (which must be a chan), returning a
+// slice of the data. If ch is a 'T chan' then the return value is of type
+// []T inside the returned interface.
+// A typical call would be sl := ChanToSlice(ch).([]int)
+func chanToSlice(ch interface{}) interface{} {
+	chv := reflect.ValueOf(ch)
+	slv := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(ch).Elem()), 0, 0)
+	for {
+		v, ok := chv.Recv()
+		if !ok {
+			return slv.Interface()
+		}
+		slv = reflect.Append(slv, v)
+	}
+}
+
+func checkForm(requires []string, c echo.Context) error {
+	for _, check := range requires {
+		if c.FormValue(check) == "" {
+			return c.String(http.StatusBadRequest, check+" not provided")
+		}
+	}
+	return nil
+}
+
+func listBoxImages(c echo.Context) error {
+	checkForm([]string{
+		"box-name",
+	}, c)
+
+	ch := make(chan struct{})
+
+	images := d.KeysPrefix("box/"+c.FormValue("box-name")+"/images/", ch)
+
+	imageSlice := chanToSlice(images).([]string)
+
+	close(ch)
+
+	return c.JSON(http.StatusOK, imageSlice)
+}
+
+func downloadBoxImage(c echo.Context) error {
+
+	checkForm([]string{
+		"box-name",
+		"file-name",
+	}, c)
+
+	return download(c, "box/"+c.FormValue("box-name")+"/images/"+c.FormValue("file-name"))
+
+}
+
+func download(c echo.Context, path string) error {
+	return c.File(path)
+}
+
+func uploadBoxImage(c echo.Context) error {
+
+	checkForm([]string{
+		"box-name",
+	}, c)
+
+	// Read form fields
+	path := "/box/" + c.FormValue("box-name") + "/images/"
+
+	return upload(c, path)
+}
+
+func upload(c echo.Context, directory string) error {
+	//-----------
+	// Read file
+	//-----------
+
+	checkForm([]string{
+		"file",
+	}, c)
+	// Source
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	fullFile, err := ioutil.ReadAll(src)
+
+	mType := mimetype.Detect(fullFile)
+	if err != nil {
+		return err
+	}
+
+	hash := md5.Sum(fullFile)
+
+	location := directory + hex.EncodeToString(hash[:]) + mType.Extension()
+
+	err = d.Write(location, fullFile)
+	if err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("File %s uploaded successfully to location %s", file.Filename, location))
 }
--- test.sh
+++ test.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-__jwt=$(curl -s -X POST -d 'username=jon' -d 'password=shhh!' localhost:8083/login | jq -r '.token')
+__username="$(yq -r '.username' <"${HOME}/.corrugation-backend.yaml")"
+__password="$(yq -r '.password' <"${HOME}/.corrugation-backend.yaml")"
 
-curl localhost:8083/api/info -H "Authorization: Bearer ${__jwt}"
+__jwt=$(curl -s -X POST -d "username=${__username}" -d "password=${__password}" localhost:8083/login | jq -r '.token')
+
+__curl() {
+    __path="${1}"
+    shift
+    curl "localhost:8083${__path}" "${@}" -H "Authorization: Bearer ${__jwt}"
+
+    echo
+    echo
+}
+
+# __curl '/api/info'
+
+__curl '/api/upload/box-image' -F 'file=@./screen.png' -F 'box-name=Network Cables'
+
+__curl '/api/list/box-image' -X GET -F 'box-name=Network Cables'
+
+__curl '/api/download/box-image' -X GET -F 'box-name=Network Cables'
 
 exit
